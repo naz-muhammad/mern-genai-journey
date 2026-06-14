eef3c48036d35dc28553ccb22f210f4ee61923d7
naz-muhammad <nazmuhammad.dev@gmail.com>
2026-06-14 10:36:19 -0700

    loop question are done to some extent

---
+++ 03-JavaScript/loop/prime.js
@@ -0,0 +1,17 @@
+let num = Number(prompt("Enter a num: "))
+let isPrime = true
+
+for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+        isPrime = false
+        break
+    }
+    
+}
+
+if (isPrime) {
+    console.log("Prime")
+}
+else{
+    console.log("Not Prime")
+}
