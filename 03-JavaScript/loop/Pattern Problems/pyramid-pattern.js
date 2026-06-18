80c986861b468d3b4f834ff339c7b470fc647861
naz-muhammad <nazmuhammad.dev@gmail.com>
2026-06-18 10:31:02 -0700

    done the loop questions and some pattern

---
+++ 03-JavaScript/loop/Pattern Problems/pyramid-pattern.js
@@ -0,0 +1,32 @@
+
+// let n = 5;
+
+// for (let i = 1; i <= n; i++) {
+
+//     let spaces = "";
+//     let stars = "";
+
+//     // spaces
+//     for (let j = 1; j <= n - i; j++) {
+//         spaces += " ";
+//     }
+
+//     // stars
+//     for (let j = 1; j <= 2 * i - 1; j++) {
+//         stars += "*";
+//     }
+
+//     console.log(spaces + stars);
+// }
+
+
+let nums = [10, 20, 30];
+
+console.log(Array.isArray(nums));
+
+let student = {
+    name: "Ali",
+    age: 20
+};
+
+console.log(Array.isArray(student));
--- 03-JavaScript/loop/factorial.js
+++ 03-JavaScript/loop/factorial.js
@@ -1,7 +1,13 @@
-let num = 3;
-let factorial = 1;
-for (let i = 1; i <= num; i++) {
-    factorial = factorial * i
-}
 
-console.log(factorial)
+let num = Number(prompt("Enter a num: "))
+let factorial = 1
+
+for (let i = num; i >= 1; i--) {
+    console.log(`${i} X ${factorial} = ${factorial * i}`);
+
+    factorial *= i
+
+}
+console.log(" ");
+
+console.log(`factorial: ${factorial}`);
