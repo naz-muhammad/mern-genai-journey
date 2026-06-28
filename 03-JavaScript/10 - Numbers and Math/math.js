a207b6d26dc8ceea2459d881f8170fe49ce9631c
naz-muhammad <nazmuhammad.dev@gmail.com>
2026-06-28 11:46:20 -0700

    understand the concept of math and numbers

---
+++ 03-JavaScript/10 - Numbers and Math/math.js
@@ -0,0 +1,13 @@
+// console.log(Math.PI)
+// console.log(Math.abs(-6))
+// console.log(Math.ceil(7.990))  // 8
+// console.log(Math.round(7.990))  // 8
+// console.log(Math.floor(7.990))  // 7
+// console.log(Math.pow(8,2))
+// console.log(Math.max(8, 3, 6))
+// console.log(Math.min(8, 3, 6))
+
+const min = 10;
+const max = 20 ;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
---
+++ 03-JavaScript/10 - Numbers and Math/number.js
@@ -0,0 +1,12 @@
+let number = 400 ; 
+// console.log(typeof number);
+
+let anotherNumber = new Number(400590)
+// console.log(anotherNumber);
+
+console.log(typeof anotherNumber.toExponential(2))
+console.log(typeof anotherNumber.toFixed(1))
+console.log(typeof anotherNumber.toLocaleString('ur-PK'))
+console.log(typeof anotherNumber.toPrecision(4))
+console.log(typeof anotherNumber.toString())
+console.log(typeof anotherNumber.valueOf())
