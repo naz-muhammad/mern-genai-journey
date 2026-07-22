cabc387e4e174479c286df722bbb8dac2ef58947
naz-muhammad <nazmuhammad.dev@gmail.com>
2026-07-22 00:35:51 -0700

    understand the flow of structure

---
+++ 04 - react/01-vite-setup-understanding/src/App.jsx
@@ -0,0 +1,17 @@
+function MyButton() {
+  return (
+    <button>I am a button</button>
+  )
+}
+
+
+function App() {
+ return (
+    <>
+      <h1>Hello g this is Naz muhammad with an new journey of React</h1>
+      <MyButton/>
+    </>
+  )
+}
+
+export default App
