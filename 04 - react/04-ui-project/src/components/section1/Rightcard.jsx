1c053d88a4becd6196a4f960f00bf38de30bc1fc
naz-muhammad <nazmuhammad.dev@gmail.com>
2026-07-25 03:27:06 -0700

    done static ui using react

--- 04 - react/04-ui-project/src/components/section1/Rightcard.jsx
+++ 04 - react/04-ui-project/src/components/section1/Rightcard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Rightcardcontent from './Rightcardcontent'
 
 const Rightcard = () => {
   return (
-    <div>
-      I am right card
+    <div className='h-full w-[35%] relative shrink-0 overflow-hidden rounded-4xl'>
+      <img className='h-full  object-cover' src="https://plus.unsplash.com/premium_photo-1661769159995-f3af0089875f?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="alt"/>
+      <Rightcardcontent />
     </div>
   )
 }
--- 04 - react/04-ui-project/src/components/section1/Rightcardcontent.jsx
+++ 04 - react/04-ui-project/src/components/section1/Rightcardcontent.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { ArrowRight } from "lucide-react";
 
 const Rightcardcontent = () => {
   return (
-    <div>
-      
+    <div className='absolute top-0 flex flex-col justify-between h-full w-full rounded-2xl p-8'>
+      <h4 className='bg-white w-10 text-xl h-10 rounded-full grid place-items-center font-bold'>1</h4>
+      <div className='flex flex-col gap-6'>
+        <p className='text-white text-xl'>Lorem, ipsum dolor sit amet conse ctetur adipis icing elit. Dolo rum, ad vitae! Eligendi!</p>
+        <div className='flex justify-between'>
+          <button className='bg-yellow-400 px-4 py-2 rounded-full font-medium border-none text-white'>Underbanked</button>
+          <button className='text-white'><ArrowRight /></button>
+        </div>
+      </div>
     </div>
   )
 }
